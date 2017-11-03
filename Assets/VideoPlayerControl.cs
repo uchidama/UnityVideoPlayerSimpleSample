8426c51bb1c05543fbd5e40a4ddcd3e6ba9e0ee2
Shin Ishimaru <shinishimaru@gmail.com>
2017-11-04 03:37:17 +0900

    Add VideoPlayerControl.cs

---
+++ Assets/VideoPlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space)) {
+
+			VideoPlayer videoPlayer = this.GetComponent<VideoPlayer> ();
+
+			if (videoPlayer.isPlaying) {
+				videoPlayer.Pause();
+			} else {
+				videoPlayer.Play ();
+			}
+
+		}
+	}
+}
